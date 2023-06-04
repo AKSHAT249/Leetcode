f109fac568cb2447343d86270591886638b07459
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2023-06-04 14:58:15 +0530

    Update 2718. Sum of Matrix After Queries.py

--- Weekly_Contest_348/2718. Sum of Matrix After Queries.py
+++ Weekly_Contest_348/2718. Sum of Matrix After Queries.py
@@ -1,34 +1,24 @@
-// Brute Force Approach ----->  TLE(Time Limit Exceeded) <------- 
-
 class Solution:
     def matrixSumQueries(self, n: int, queries: List[List[int]]) -> int:
         
+        rows = set()
+        cols = set()
         
+        result = 0
         
-        matrix = []
-        k=0
-        for i in range(n):
-            liste = []
-            for j in range(n):
-                liste.append(0)
-            matrix.append(liste)
-     
-     
-        for values in queries:
-            typ, index, value = values[0], values[1], values[2]
-            if typ==0:
-                for i in range(n):
+        for values in queries[::-1]:
+            types, index, value = values[0], values[1], values[2]
+            if types==1:
+                if index not in cols:
+                    cols.add(index)
+                    result+= value*(n-len(rows))
                     
-                    matrix[index][i]=value
+                    
+            else:
+                if index not in rows:
+                    rows.add(index)
+                    result+= value*(n-len(cols))
                 
-            elif typ==1:
-                for i in range(n):
-                    
-                    matrix[i][index]=value
-               
+        return result
         
-        summ = 0
-        for i in range(n):
-            for j in range(n):
-                summ+=matrix[i][j]
-        return summ
+        
